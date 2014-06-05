3277ca4f985fbd3a466a1107e58d1977bc656e5d
Anton Pawlik <anton.pawlik@gmail.com>
2014-06-05 14:55:05 +0200

    config fix

---
+++ README.md
@@ -0,0 +1,1 @@
+http://bhf-demo.herokuapp.com/
--- config/initializers/bhf.rb
+++ config/initializers/bhf.rb
@@ -1,7 +1,7 @@
 module Bhf
   class Engine < Rails::Engine
     
-    config.on_login_fail = :admin_url
+    config.bhf.on_login_fail = :admin_url
     
   end
 end
